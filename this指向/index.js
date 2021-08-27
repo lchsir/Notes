fd36f8b3477ea4c078c2c81c7ec35a4acbadb3ee
刘春辉 <liuchunhui@qiyee-tech.com>
2021-08-27 11:11:21 +0800

    事件委托

--- this指向/index.js
+++ this指向/index.js
@@ -1,5 +1,5 @@
 
-// 定义：只能在函数内部使用
+// this定义：只能在函数内部使用
 // 指向规则：判断有没有调用者，如果有调用者指向调用者，没有指向window，严格模式下指向undefined
 
 
@@ -26,6 +26,51 @@ test(f2, 1000)
 test(function(n){
   console.log(n)
 }, 1000)
+
+
+// 改变this的指向apply、call、bind
+
+// apply、call 
+// a.apply(b)  // 将a中的成员放到b中，this直接指向b
+function o1(name, color) {
+  this.name = name
+  this.color = color
+}
+var a1 = new o1('ssa', 'red')
+var o2 = {};
+// 需求：o2想调用o1里边的东西
+// o1对象在o2中执行，此时o1 指向 02
+o1.apply(o2, ['o2', 'o2'])
+o1.call(o2, 'o2', 'o2')
+o2.name
+
+// !!!注: bind指向发生变化后，就不能再改回去了
+var num = 10;
+function test() {
+ return this.num
+}
+
+var obj = {
+  num: 5,
+  fun: test()
+}
+
+var obj2 = {
+  num: 999,
+  fun: test()
+}
+
+var x2 = test.bind(obj) // x2() = 5, this的指向变更为obj, this.num = obj.num
+var x2 = test.bind(obj2) // x2() = 999, this的指向变更为obj2, this.num = obj2.num
+x2();
+console.log(x2.call(obj2)) // 绑定之后是不能再改变this的指向的
+
+
+// !!! 自执行函数内部的this指向window
+
+
+
+
 
 
 
---
+++ 事件冒泡与事件捕获/index.js
@@ -0,0 +1,64 @@
+// 1、事件冒泡 （由下而上）
+{/* <div>
+  div1 ---
+  <div>
+    div2 ---
+    <div>
+      div3 ---
+    </div>
+  </div>
+</div> */}
+
+// 点击div3 （由下而上）
+// 阻止冒泡：event.stopPropagation()
+
+
+// 2、事件捕获 （自上向下）
+
+// e.target 和 e.currentTarget 区别
+// target：  触发谁指向的就是谁，就作用到谁（触发者）
+// currentTarget：  绑定的事件元素，绑定的谁就指向谁（绑定者）会向下传播
+
+
+// 3、事件委托
+
+{/* <ul id="list">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul> */}
+
+// 传统的一个一个触发
+var list = document.getElementById('list')
+var li = list.getElementsByTagName('li')
+for (var i = 0; i < li.length; i++) {
+  li[i].onclick = function() {
+    this.style.color = 'red'
+  }
+}
+
+// 借助事件委托，将事件绑定到父节点，点击节点通过事件冒泡到夫并处理
+var list = document.getElementById('list')
+list.onclick = function(e) {
+  if (e.target.nodeName === 'LI') {
+    e.target.style.color = 'red'
+  }
+}
+
+/* 事件捕获和事件冒泡机制 */
+
+// 1、事件捕获
+// 当一个事件触发后,从Window对象触发,不断经过下级节点,直到目标节点。在事件到达目标节点之前的过程就是捕获阶段。所有经过的节点,都会触发对应的事件
+
+// 2、目标阶段
+// 当事件不断的传递直到目标节点的时候，最终在目标节点上触发这个事件，就是目标阶段
+
+// 3、事件冒泡
+// 当事件到达目标节点后，会沿着捕获阶段的路线原路返回。同样，所有经过的节点,都会触发对应的事件
+
+
+
+
+
+
